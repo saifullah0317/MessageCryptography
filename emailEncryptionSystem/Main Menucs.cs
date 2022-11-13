af9219d2e611d38869532ecfcd1187b15ff94928
Sayyed Ahmed <sayyedahmed457@gmail.com>
2022-11-13 18:32:49 +0500

    Email-Conversation-interface
    For-Receiver

---
+++ emailEncryptionSystem/Main Menucs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace emailEncryptionSystem
+{
+    public partial class Main_Menucs : Form
+    {
+        
+        public Main_Menucs()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Main_Menucs_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Email To  ";
+        }
+    }
+}
